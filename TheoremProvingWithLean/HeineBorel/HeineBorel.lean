237c31f578c121bde02b48534d9fd9ba89712ddc
BSCurtis12345 <bscurtis@virginmedia.com>
2025-11-26 22:21:23 +0000

    Copied and pasted changes to Icc_Compact_R to main HeineBorel file

--- TheoremProvingWithLean/HeineBorel/HeineBorel.lean
+++ TheoremProvingWithLean/HeineBorel/HeineBorel.lean
@@ -13,25 +13,53 @@ Mathlib results used:
 -/
   rw [isCompact_iff_finite_subcover]
 
+  -- Let {U_i} for i ∈ I be an open cover, A be the set of x's s.t. [a,x] has a finite subcover
   intros I U hUopen hcover
-
   let A : Set ℝ := {x ∈ Set.Icc a b | ∃ (t : Finset I), Set.Icc a x ⊆ ⋃ i ∈ t, U i}
 
-  have haInA : a ∈ A := by
-    simp [A] -- note: uses Set.mem_sep_iff, Set.mem_Icc
+  have hAsubIcc : A ⊆ Set.Icc a b := by
+  -- Proves A is a subset of [a,b] for later convenience
+    intros x hx
+    exact hx.1
 
+  have haInA : a ∈ A := by
+  -- Proves a ∈ A
+    simp [A] -- note: uses Set.mem_sep_iff, Set.mem_Icc
     constructor
     · exact h
+    · have ha : a ∈ Set.Icc a b := by simpa
+      apply elt_in_cover_elt U hcover a at ha
+      rcases ha with ⟨j, hj⟩
+      let J : Finset I := {j}
+      use J
+      use j
+      simp [J, hj]
 
-    · have haCover : a ∈ ⋃ i, U i := by
-        apply hcover
-        simpa
+  have hsup : sSup A ∈ Set.Icc a b := by
+  -- Proves sup A ∈ [a,b]
+    simp
+    constructor
+    · apply le_csSup
+      apply subset_Icc.bdd_above hAsubIcc
+      exact haInA
+    · apply csSup_le
+      · use a
+      · intros y hy
+        apply hAsubIcc at hy
+        exact hy.2
 
-      rw [Set.mem_iUnion] at haCover
-      rcases haCover with ⟨j, hj⟩
-      let J : Set I := {x | x=j}
-      use J -- problem: switching to Finset
-
-
-
-done
+  apply elt_in_cover_elt U hcover (sSup A) at hsup -- sup A ∈ U_j for some j
+  rcases hsup with ⟨j, hj⟩
+  have hcNhd : ∃ ε > 0, Set.Ioo (sSup A - ε) (sSup A + ε) ⊆ U j := by
+  -- Proves there is an ε > 0 with (sup A - ε, sup A + ε) ⊆ U_j
+    sorry -- show this later (from openness of U j)
+  rcases hcNhd with ⟨ε, hε⟩
+  have hx : ∃ x ∈ A, (sSup A - ε) < x := by
+  -- Proves there is an x > sup A - ε in A
+    apply exists_lt_of_lt_csSup
+    · use a
+    · simp [hε]
+  rcases hx with ⟨x, hx⟩
+  have hunion : Set.Icc a x ∪ Set.Ioo (sSup A - ε) (sSup A + ε) = Set.Ico a (sSup A + ε) := by
+  -- Proves [a,x] ∪ (sup A - ε, sup A + ε) = [a, sup A + ε)
+    sorry -- do this later
