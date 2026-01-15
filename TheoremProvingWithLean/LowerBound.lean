468ac2f5fc783635e155adb3ad8dde61152bb90b
BSCurtis12345 <bscurtis@virginmedia.com>
2026-01-15 16:05:57 +0000

    Finished ball = pow of interval

--- TheoremProvingWithLean/LowerBound.lean
+++ TheoremProvingWithLean/LowerBound.lean
@@ -3,6 +3,7 @@ import TheoremProvingWithLean.Norms
 
 set_option linter.style.longLine false
 set_option linter.flexible false
+set_option linter.style.emptyLine false
 
 abbrev Rn (n : ℕ) := Fin n → ℝ
 abbrev unit_Icc_pow (n : ℕ) := {x : Rn n | ∀ i, x i ∈ Set.Icc (-1) 1}
@@ -16,15 +17,26 @@ lemma closed_ball_sup_eq_pow : closed_ball_sup n = unit_Icc_pow n := by
   /-
   This lemma proves that the closed unit ball w.r.t. the supremum norm in ℝⁿ is equal to [-1,1]ⁿ.
   -/
-  ext x
+  ext x -- Prove by extensionality
   constructor
-  · intro hx
-    simp [norm, nnnorm] at hx
-    intro i
+  · -- Assume x is in the closed ball
+    intro hx
+    simp [norm, nnnorm] at hx -- Unpack the definition of the ball and sup norm
+    intro i -- Let i be an arbitrary natural < n
+
+    -- Specialise hx for i, unpack definitions further using simp
     specialize hx i
     simp
-    replace hx : |x i| ≤ 1 := by simpa [hx]
-    exact abs_le.mp hx
-  · sorry
+
+    exact abs_le.mp hx -- Result follows precisely from the definition of the abs value
+  · -- Assume x is in the interval to the nth power
+    -- Unpack definitions of ball and norm and let i be arbitrary
+    intro hx
+    simp [norm, nnnorm]; intro i
+
+    -- Unpack definition of power of interval, apply to i, conclude with converse to above
+    simp at hx
+    specialize hx i
+    exact abs_le.mpr hx
 
   done
